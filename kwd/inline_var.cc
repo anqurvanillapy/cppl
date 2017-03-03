53b40c38b7046428d8899ec402d2763a1fa6cb5a
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-03-03 09:35:33 +0800

    updated inline var but it seems uncompilable (due to c++17 and insufficient g++ version)

--- kwd/inline_var.cc
+++ kwd/inline_var.cc
@@ -9,13 +9,43 @@
  *  *one* of them.
  *
  *  Note: Compile with -std=c++17.
+ *
+ *  XXX: This seems not compilable on my g++ 5.4.0.
  */
 
 #include <iostream>
 
+/// Until C++17, it can be done via this trick with template.
+template<class Dummy>
+struct _Foo {
+    static std::string hi;
+};
+
+/// Static member variable should be initialized outside the class.
+template<class Dummy>
+std::string _Foo<Dummy>::hi = "Hi! I'm Foo.";
+
+/// Type alias via using was introduced since C++11.
+using Foo = _Foo<void>; // `Foo::hi' is now allowed
+
+/// Use the inline variable outside the class.
+struct Bar {
+    static std::string const hi = "Hi! I'm Bar.";
+};
+
+inline std::string Bar::hi = "Hi! I'm Bar.";
+
+/// It can also be defined in the class definition.
+struct Baz {
+    static inline std::string hi = "Hi! I'm Baz.";
+};
+
 int
 main(int argc, const char *argv[])
 {
-    // TODO
+    std::cout << Foo::hi << std::endl;
+    std::cout << Bar::hi << std::endl;
+    std::cout << Baz::hi << std::endl;
+
     return 0;
 }
