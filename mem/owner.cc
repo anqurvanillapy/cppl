07ad2edd8be3a7e40aa7c76de366a5ae05510869
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-12-30 20:10:42 +0800

    updated ownership: make a singleton

--- mem/owner.cc
+++ mem/owner.cc
@@ -9,32 +9,42 @@
 
 #include <iostream>
 #include <type_traits>
+#include <cassert>
 
 // Annotate in the header that the return value of the function is probably
 // a pointer that will not be deleted on the heap, passing the ownership to the
 // caller.
-template <class T, class =std::enable_if_t<std::is_pointer<T>::value>>
+template <class T, class = std::enable_if_t<std::is_pointer<T>::value>>
 using owner = T;
 
 class foo {
-	foo()
+public:
+	static owner<foo*>
+	get()
 	{
-		std::cout << "foo ctor" << std::endl;
+		if (!i_) i_ = new foo;
+		return i_;
 	}
 
-	friend owner<foo*> get_foo();
+	static void
+	do_something()
+	{
+		assert(i_ != nullptr && "singleton is not alive");
+	}
+private:
+	foo() = default;
+
+	static foo* i_;
 };
 
-owner<foo*>
-get_foo()
-{
-	return new foo;
-}
+foo* foo::i_{};
 
 int
 main()
 {
-	auto p = get_foo();
+	auto p = foo::get();
+	p->do_something();
 	delete p;
+
 	return 0;
 }
