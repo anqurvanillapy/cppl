8508e13554d29aef9ab2b97acad21ab0da270a47
AnqurVanillapy <anqurvanillapy@gmail.com>
2018-03-30 13:50:53 +0800

    updated mymemcpy: optimization

--- reimpl/mymemcpy.c
+++ reimpl/mymemcpy.c
@@ -2,38 +2,112 @@
  *  mymemcpy
  *  ========
  *
- *  A re-implementation of memcpy without using libraries functions.
+ *  A re-implementation of memcpy without using libraries functions.  Test with
+ *  -DTEST_64 or -DTEST_32 for some optimizations.
+ *
+ *  Note: Compile with -O3 with the naive implementation `mymemcpy` will enable
+ *  loop unrolling and SIMD instructions usage in GCC 8, which might be much
+ *  quicker and closer to the library one `memcpy`.
  */
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h> // for size_t in gcc, I'm sorry
+#include <stddef.h>
+#include <assert.h>
 
-void *
-mymemcpy(void *dest, const void *src, size_t n)
+#define SIZ (1024 * 1024 * 128)
+
+void
+mymemcpy(void* dst, const void* src, size_t n)
 {
-    char *pd, *ps;
-    // Use n directly for size_t might be 64-bit, thus unsafe for an int cast.
-    for (pd = (char *)dest, ps = (char *)src; n > 0; --n, ++pd, ++ps) *pd = *ps;
-    return pd;
+	size_t i;
+	unsigned char* d = (unsigned char*)dst;
+	unsigned char* s = (unsigned char*)src;
+	for (i = 0; i < n; ++i) {
+		*d++ = *s++;
+	}
 }
 
-typedef struct {
-    int val;
-} foo_t;
+void
+mymemcpy32(void* dst, const void* src, size_t n)
+{
+	unsigned char* bd = (unsigned char*)dst;
+	unsigned char* bs = (unsigned char*)src;
+
+	while (((size_t)bd & 0x3) != 0) {
+		*bd++ = *bs++;
+		--n;
+	}
+
+	unsigned int* ld = (unsigned int*)dst;
+	unsigned int* ls = (unsigned int*)src;
+
+	while (n >= 4) {
+		*ld++ = *ls++;
+		n -= 4;
+	}
+
+	if (n) {
+		bd = (unsigned char*)ld;
+		bs = (unsigned char*)ls;
+
+		while (n) {
+			*bd++ = *bs++;
+			--n;
+		}
+	}
+}
+
+void
+mymemcpy64(void* dst, const void* src, size_t n)
+{
+	unsigned char* bd = (unsigned char*)dst;
+	unsigned char* bs = (unsigned char*)src;
+
+	while (((size_t)bd & 0x7) != 0) {
+		*bd++ = *bs++;
+		--n;
+	}
+
+	size_t* qd = (size_t *)dst;
+	size_t* qs = (size_t *)src;
+
+	while (n >= 8) {
+		*qd++ = *qs++;
+		n -= 8;
+	}
+
+	if (n) {
+		bd = (unsigned char*)qd;
+		bs = (unsigned char*)qs;
+
+		while (n) {
+			*bd++ = *bs++;
+			--n;
+		}
+	}
+}
 
 int
-main(int argc, const char *argv[])
+main()
 {
-    foo_t *foo, *bar;
+	void *a, *b;
+	char *aa, *bb;
 
-    foo = (foo_t *)malloc(sizeof(foo_t));
-    bar = (foo_t *)malloc(sizeof(foo_t));
+	a = malloc(SIZ);
+	b = malloc(SIZ);
 
-    foo->val = 42;
-    mymemcpy(bar, foo, sizeof(bar));
-    printf("bar->val=%d\n", bar->val);
+	aa = (char*)a;
+	aa[SIZ - 1] = 'a';
+	bb = (char*)b;
 
-    return 0;
+#if defined(TEST_64)
+	mymemcpy64(b, a, SIZ);
+#elif defined(TEST_32)
+	mymemcpy32(b, a, SIZ);
+#else
+	mymemcpy(b, a, SIZ);
+#endif /* TEST_64 */
+
+	assert(bb[SIZ - 1] == 'a');
+	return 0;
 }
-
