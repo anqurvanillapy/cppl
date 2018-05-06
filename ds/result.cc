368e836663d308d2f08c89e000289937cb7a0cd9
AnqurVanillapy <anqurvanillapy@gmail.com>
2018-05-06 14:02:20 +0800

    updated result: updated some Rust-like APIs, but finally not a totally zero-cost abstraction :( too sad

--- ds/result.cc
+++ ds/result.cc
@@ -8,8 +8,10 @@
  */
 
 #include <iostream>
+#include <functional>
 #include <variant>
 #include <optional>
+#include <cassert>
 
 template <typename T, typename E>
 class result {
@@ -22,39 +24,83 @@ public:
 	result(result<T, E>&&)                  = delete;
 	result& operator=(result<T, E>&&)       = delete;
 
-	explicit constexpr result(const T& v)
+	template <bool Ok = true>
+	explicit constexpr result(const std::enable_if_t<Ok, T>& v)
 		: v_{v}
+		, ok_{Ok}
 	{
+		/* nop */
 	}
 
-	explicit constexpr result(const E& e)
+	template <bool Ok = false>
+	explicit constexpr result(const std::enable_if_t<!Ok, E>& e,
+							  const std::nullopt_t&)
 		: v_{e}
+		, ok_{Ok}
 	{
+		/* nop */
 	}
 
-	explicit constexpr result(const E& e, const std::nullopt_t&)
-		: v_{e}
+	auto&
+	unwrap() const
 	{
+		if (ok_) {
+			return std::get<0>(v_);
+		}
+		throw err_();
 	}
 
-	constexpr auto&
-	val() const noexcept
+	auto&
+	unwrap_err() const
 	{
-		return std::get<T>(v_);
+		if (!ok_) {
+			return err_();
+		}
+		throw std::get<0>(v_);
 	}
 
-	constexpr auto&
-	err() const noexcept
+	auto&
+	operator*() const
 	{
-		return std::get<E>(v_);
+		return unwrap();
+	}
+
+	template <typename F>
+	result<T, E>
+	and_then(F f) const
+	{
+		try {
+			auto v = std::get<0>(v_);
+			return f(v);
+		} catch (...) {
+			auto e = err_();
+			return f(e);
+		}
 	}
 
 	operator bool() const noexcept
 	{
-		return std::get_if<T>(&v_);
+		return ok_;
 	}
 private:
-	std::variant<T, E> v_;
+	constexpr auto&
+	err_() const noexcept
+	{
+		if constexpr (same_type_()) {
+			return std::get<0>(v_);
+		} else {
+			return std::get<1>(v_);
+		}
+	}
+
+	using same_type_ = std::is_same<T, E>;
+
+	typename std::conditional<
+		std::is_same_v<T, E>,
+		std::variant<T>,
+		std::variant<T, E>
+	>::type v_;
+	const bool ok_;
 };
 
 template <typename T, typename E>
@@ -68,14 +114,20 @@ template <typename T, typename E>
 constexpr auto
 err(const E& e) noexcept
 {
-	return result<T, E>(e);
+	return result<T, E>(e, std::nullopt);
 }
 
 template <typename T, typename E>
 constexpr auto
-err(const E& e, const std::nullopt_t& nil) noexcept
+match(const result<T, E>& res) noexcept
 {
-	return result<T, E>(e, nil);
+	return [&] (std::function<void(T)> f, std::function<void(E)> g) {
+		if (res) {
+			f(res.unwrap());
+		} else {
+			g(res.unwrap_err());
+		}
+	};
 }
 
 result<int, std::string>
@@ -88,16 +140,50 @@ foo(bool c)
 	}
 }
 
+result<std::string, std::string>
+bar(bool c)
+{
+	if (c) {
+		return ok<std::string, std::string>("good");
+	} else {
+		return err<std::string, std::string>("bad");
+	}
+}
+
 int
 main()
 {
-	auto a = foo(42);
-	std::cout << std::boolalpha << "a (" << a << ") = ";
-	std::cout << a.val() << std::endl;
+	auto a = foo(1 + 1 == 2);
+	assert(a);
+	assert(a.unwrap() == 42);
 
 	auto b = foo(2 + 2 == 5);
-	std::cout << std::boolalpha << "b (" << b << ") = ";
-	std::cout << b.err() << std::endl;
+	assert(!b);
+	assert(b.unwrap_err() == "failed");
+
+	auto c = bar(1);
+	assert(c);
+	assert(c.unwrap() == "good");
+
+	auto d = bar(0);
+	assert(!d);
+	assert(d.unwrap_err() == "bad");
+
+	std::cout << "sizeof a: " << sizeof a << std::endl;
+	std::cout << "sizeof b: " << sizeof b << std::endl;
+
+	auto e = ok<int, int>(42);
+	auto f0 = [] (int v) { return ok<int, int>(v + 1); };
+	auto f1 = [] (int v) { return err<int, int>(v - 1); };
+	auto ea = e.and_then(f0).and_then(f0);
+	auto eb = e.and_then(f1).and_then(f1);
+	assert(ea.unwrap() == 44);
+	assert(eb.unwrap_err() == 40);
+
+	match(e)(
+		[] (int v) { std::cout << v + v << std::endl; },
+		[] (int v) { std::cout << v - v << std::endl; }
+	);
 
 	return 0;
 }
