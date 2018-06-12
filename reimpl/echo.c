9358724928a2d1211d5a8372fdfcf05f596d7282
AnqurVanillapy <anqurvanillapy@gmail.com>
2018-06-13 00:02:19 +0800

    new reimpl: echo

---
+++ reimpl/echo.c
@@ -0,0 +1,34 @@
+/**
+ *  echo
+ *  ====
+ *
+ *  The `echo` command.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int
+main(int argc, const char *argv[])
+{
+	(void)argc;
+	int nl = 0;
+
+	if (*++argv && !strcmp(*argv, "-n")) {
+		++argv;
+		nl = 1;
+	}
+
+	while (*argv) {
+		fputs(*argv, stdout);
+		if (*++argv) {
+			putchar(' ');
+		}
+	}
+
+	if (!nl) {
+		putchar('\n');
+	}
+
+	return 0;
+}
