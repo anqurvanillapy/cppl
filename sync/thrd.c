f62f64dd5d5845a4d1f72c48815528207f312476
AnqurVanillapy <anqurvanillapy@gmail.com>
2018-08-05 21:56:13 +0800

    updated c11 thrd: numactl cpubind

--- sync/thrd.c
+++ sync/thrd.c
@@ -6,7 +6,11 @@
  *
  *  Note: It is supported in glibc version >= 2.28, which is not popular enough
  *  to run this code as soon as possible.  One can go check out musl-gcc for
- *  compling this demo.
+ *  compling this demo.  The use of `thrd_yield` can be indicated via re-binding
+ *  threads for the process by the following command:
+ *
+ *      $ musl-gcc thrd.c -std=c11 -Wall -Wextra -pedantic -Ofast
+ *      $ numactl --physcpubind=+1 ./a.out
  */
 
 #include <stdio.h>
@@ -14,16 +18,17 @@
 #include <threads.h>
 #include <stdatomic.h>
 
+#define N 5
 _Atomic long i = 0;
 
 int
 foo(void* arg)
 {
-	long n = (long)arg;
+	(void)arg;
 	do {
 		thrd_yield();
-		printf("%ld", atomic_load(&i));
-	} while (atomic_load(&i) < n);
+		printf("foo: %ld\n", atomic_load(&i));
+	} while (atomic_load(&i) < N);
 
 	return thrd_success;
 }
@@ -31,10 +36,11 @@ foo(void* arg)
 int
 bar(void* arg)
 {
-	for (int j = 0; j < (long)arg; ++j) {
-		sleep(1);
+	(void)arg;
+	for (int j = 0; j < N; ++j) {
+		thrd_yield();
+		printf("bar: inc i\n");
 		atomic_fetch_add(&i, 1);
-		++j;
 	}
 
 	return thrd_success;
@@ -47,12 +53,12 @@ main()
 	thrd_t t2;
 	int rc1, rc2;
 
-	thrd_create(&t1, foo, (void*)3l);
-	thrd_create(&t2, bar, (void*)10l);
+	thrd_create(&t1, foo, NULL);
+	thrd_create(&t2, bar, NULL);
 
 	thrd_join(t1, &rc1);
 	thrd_join(t2, &rc2);
 
-	printf("\nrc=%d, %d\n", rc1, rc2);
+	printf("rc=%d, %d\n", rc1, rc2);
 	return 0;
 }
