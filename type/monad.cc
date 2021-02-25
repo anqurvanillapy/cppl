c092099de5a2b1a3a668471098558d4592732491
anqurvanillapy <anqurvanillapy@gmail.com>
2021-02-25 18:02:07 +0800

    [ type ] Update Monad: Applicative and Kleisli composition

--- type/monad.cc
+++ type/monad.cc
@@ -1,13 +1,32 @@
 /**
- *  Monad
- *  =====
+ * Monad
+ * =====
+ *
+ * Monad is just a monoid in the endofunctor category, what's the problem?
+ *
+ * The following code might be wrong actually ;)
  */
+
+#include <functional>
 
 #define 𝓒 typename
 
-template <𝓒> struct 𝕋 {
-    template <𝓒 𝕏> 𝕋<𝕏> pure(𝕏 x);
-    template <𝓒 𝕏> 𝕋<𝕏> μ(𝕋<𝕋<𝕏>> x);
+template <𝓒> struct Applicative {
+  template <𝓒 𝕏, 𝓒 𝕐> static Applicative<𝕐(𝕏)> Pure(std::function<𝕐(𝕏)> x);
+
+  template <𝓒 𝕏, 𝓒 𝕐>
+  static Applicative<𝕐> Seq(Applicative<𝕏> a, Applicative<𝕐(𝕏)> f);
 };
 
-int main(){}
+template <𝓒 𝔸> struct Monad : Applicative<𝔸> {
+  template <𝓒 𝕏> static Monad<𝕏> Return(𝕏 x);
+  template <𝓒 𝕏> static Monad<𝕏> μ(Monad<Monad<𝕏>> x);
+
+  template <𝓒 𝕏, 𝓒 𝕐, 𝓒 ℤ>
+  static std::function<Monad<ℤ>(𝕏)> ￮(std::function<Monad<ℤ(𝕐)>> g,
+                                      std::function<Monad<𝕐(𝕏)>> f) {
+    return [=](𝕏 x) { return μ(Seq(f(x), Pure(g))); };
+  }
+};
+
+int main() {}
